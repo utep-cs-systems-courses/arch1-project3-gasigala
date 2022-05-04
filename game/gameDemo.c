cec1b457bf68b844b507c548ea44946dbf9eac53
Gregorio Sigala <gregoriosigala@Gregorios-MacBook-Pro.local>
2022-05-04 16:21:10 -0600

    fixed program to draw shape correctly

--- game/gameDemo.c
+++ game/gameDemo.c
@@ -49,7 +49,7 @@ void wdt_c_handler()
   static int secCount = 0;
 
   secCount ++;
-  if (secCount >= 10) {		// 10/sec
+  if (secCount >= 500) {		// 10/sec
     secCount = 0;
     redrawScreen = 1;
   }
@@ -127,7 +127,6 @@ update_shape()
   if (two){
     if(current_state != sw){
       draw_background2();
-      draw_eye(25,25);
       current_state = 2;
     }
     sw = 2;
@@ -140,28 +139,35 @@ update_shape()
   }
 
   jumpTable(sw);
-  
+  //sw =0;
   for (int i = 0; i < 10; i++) {
-    fCol += i;
-    // draw_background();
-    draw_eye(fRow, fCol);
-    if(fCol >= screenHeight){
-      fCol = 50;
+    fRow += 10;
+    draw_eye(fCol, fRow);
+    if(fRow >= screenHeight){
+      fRow = 50;
     }
     //updateLocations();
     }
 }
 
+
 void
 draw_eye(int fCol, int fRow){
   // clearScreen(COLOR_WHITE);
+  fillRectangle(fCol -12, fRow-12, 48, 48, COLOR_SKY_BLUE);
   int a =0,  lenght = 24;
   int i = 12;
   int j = 0;
   for( a =0; a < lenght; a++){
+    //draw previous position as sky_blue
+    drawPixel(fCol+ a, fRow-10, COLOR_SKY_BLUE);
+    drawPixel(fCol+a, fRow+1-10, COLOR_SKY_BLUE);
     //top line of eye
     drawPixel(fCol+ a, fRow, COLOR_BLACK);
     drawPixel(fCol+a, fRow+1, COLOR_RED);
+    // draw previous position as sky blue
+    drawPixel(fCol +a, fRow+ lenght+1-10, COLOR_SKY_BLUE);
+    drawPixel(fCol+a, fRow+ lenght-10, COLOR_SKY_BLUE);
     //botom line of eye
     drawPixel(fCol +a, fRow+ lenght+1, COLOR_BLACK);
     drawPixel(fCol+a, fRow+ lenght, COLOR_RED);
@@ -169,23 +175,33 @@ draw_eye(int fCol, int fRow){
   //slanted lines
   for( a =0; a<lenght/2; a++){
     //top left 
+    drawPixel(fCol - j , fRow + j-10, COLOR_SKY_BLUE);
     drawPixel(fCol - j , fRow + j, COLOR_BLACK);
     //bottom left
+    drawPixel(fCol - i  , fRow - i +lenght-10 , COLOR_SKY_BLUE);
     drawPixel(fCol - i  , fRow - i +lenght , COLOR_BLACK);
     //top right
+    drawPixel(fCol+ lenght +j, fRow + j- 10, COLOR_SKY_BLUE);
     drawPixel(fCol+ lenght +j, fRow + j, COLOR_BLACK);
     //bottom right
+    drawPixel(fCol + lenght +j, fRow + lenght - j-10, COLOR_SKY_BLUE);
     drawPixel(fCol + lenght +j, fRow + lenght - j, COLOR_BLACK); 
     i--;
     j++;
   }
-
   //middle part of the eye
-  for( a =0; a < 8; a++){
-    for( int b =0 ; b < 8; b++){
+    for(a =0; a < 8; a++){
+    for(int b =0 ; b < 8; b++){
+      drawPixel(fCol + 8 + a, fRow +8 +b, COLOR_BLACK);
+    }
+  }
+    /*    
+  for(a =0; a < 8; a++){
+    for(int b =0 ; b < 8; b++){
       drawPixel(fCol + 8 + a, fRow + 8+ b, COLOR_BLUE);
     }
   }
+    */
 }
 
 void
@@ -204,11 +220,11 @@ draw_sun(){
 
 void
 draw_background(){
-  fillRectangle(0, 0, 128, 160/2, COLOR_SKY_BLUE); 
+  clearScreen(COLOR_SKY_BLUE); 
   draw_sun();
-  fillRectangle(0, 80, 128, 160/2, COLOR_ORANGE_RED);
+  //fillRectangle(0, 80, 128, 160/2, COLOR_ORANGE_RED);
   //this makes the road marks in screen one
-  for(int a = 0; a < 128; a+=24){
+  /* for(int a = 0; a < 128; a+=24){
     for(int b =0; b <8; b++){
       drawPixel(a+ b, 160 - 160/4, COLOR_BLACK);
       drawPixel(a+b, (160 - 160/4)+ 1, COLOR_BLACK);
@@ -222,20 +238,21 @@ draw_background(){
   else if(sw == 2){
     drawString5x7(4, 100, "two", COLOR_WHITE, COLOR_BLUE);
   }
+  */
 }
   
 
 void
 draw_background2(){
   clearScreen(COLOR_SKY_BLUE);
-  fillRectangle(0, 80, 128, 160/2, COLOR_ORANGE_RED);
+  // fillRectangle(0, 80, 128, 160/2, COLOR_ORANGE_RED);
   for(int a = 24; a< 32; a++){
     for ( int b = 50; b < 120; b++){
       drawPixel(a, b, COLOR_BLACK);
-      drawPixel(a +48, b, COLOR_BLACK);
+      drawPixel(a +70, b, COLOR_BLACK);
     }
   }
-  for (int a =0; a < 48; a++){
+  for (int a =0; a < 70; a++){
     drawPixel(a +24, 50, COLOR_BLACK);
     drawPixel(a +24, 51, COLOR_BLACK);
     drawPixel(a +24, 51, COLOR_BLACK);
